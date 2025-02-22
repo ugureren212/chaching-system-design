f5f05b95d6aa3ab0aaf14ecab22fd76cb23c3f99
Ugur Eren <ugureren212@gmail.com>
2025-02-22 19:17:35 +0000

    caching data in middleware

--- server.js
+++ server.js
@@ -23,29 +23,41 @@ async function fetchApiData(species) {
   return apiResponse.data;
 }
 
-async function getSpeciesData(req, res) {
+async function cacheData(req, res, next) {
   const species = req.params.species;
   let results;
-  let isCached = false;
-
   try {
     const cacheResults = await redisClient.get(species);
     if (cacheResults) {
-      isCached = true;
       results = JSON.parse(cacheResults);
-    } else {
-      results = await fetchApiData(species);
-      if (results.length === 0) {
-        throw "API returned an empty array";
-      }
-      await redisClient.set(species, JSON.stringify(results), {
-        EX: 180,
-        NX: true,
+      res.send({
+        fromCache: true,
+        data: results,
       });
+    } else {
+      next();
     }
+  } catch (error) {
+    console.error(error);
+    res.status(404);
+  }
+}
+async function getSpeciesData(req, res) {
+  const species = req.params.species;
+  let results;
+
+  try {
+    results = await fetchApiData(species);
+    if (results.length === 0) {
+      throw "API returned an empty array";
+    }
+    await redisClient.set(species, JSON.stringify(results), {
+      EX: 180,
+      NX: true,
+    });
 
     res.send({
-      fromCache: isCached,
+      fromCache: false,
       data: results,
     });
   } catch (error) {
@@ -54,7 +66,7 @@ async function getSpeciesData(req, res) {
   }
 }
 
-app.get("/fish/:species", getSpeciesData);
+app.get("/fish/:species", cacheData, getSpeciesData);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
